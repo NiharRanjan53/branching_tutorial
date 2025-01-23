e980cd6b173973d29e2320647532d8b2c980a304
Nihar Ranjan <niharranjanrout53@gmail.com>
2025-01-23 21:56:24 +0530

    Add decorator concept

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-
-def fun():
-    print("Hello")
+import time
+def time_calculator(fun):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = fun(*args, **kwargs)
+        end_time = time.time()
+        print(f"The execuation time of {fun.__name__} is {end_time-start_time} second.")
+        return result
+    return wrapper
+@time_calculator
+def add(a, b):
+    time.sleep(5)
+    return a+b
 
 if __name__ == "__main__":
-    fun()
+    print(add(5,3))
