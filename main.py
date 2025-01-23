54d78a2da73b24e958ba83460d83bfccd513b6ef
Nihar Ranjan <niharranjanrout53@gmail.com>
2025-01-23 23:51:11 +0530

    multyply feature added directly to main branch

--- main.py
+++ main.py
@@ -12,5 +12,11 @@ def add(a, b):
     time.sleep(5)
     return a+b
 
+@time_calculator
+def mul(a, b):
+    time.sleep(2)
+    return a*b
+
 if __name__ == "__main__":
     print(add(5,3))
+    print(mul(5, 3))
