89dbdd0b72034564f54f25a18ea1417d7cb10710
Nihar Ranjan <niharranjanrout53@gmail.com>
2025-02-03 23:01:40 +0530

    New Feture Added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 from time_calculator import time_calculator
 from subtract import subtractFun
+from newFeature import newFeature
 @time_calculator
 def add(a, b):
     time.sleep(5)
@@ -15,3 +16,4 @@ if __name__ == "__main__":
     print(add(5,3))
     print(subtractFun(5,3))
     print(mul(5, 3))
+    newFeature()
